5b7b9db8e3ad5981d839372bc13c3755d3f2654e
Ali Abou-Zahr & Nathan Grey <student@epicodus.com>
2014-03-31 11:58:04 -0700

    wikipages complete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,48 @@
 class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
+    @contact = Contact.new
     render('contacts/index.html.erb')
   end
 
   def show
     @contact = Contact.find(params[:id])
     render('contacts/show.html.erb')
+  end
+
+
+  def create
+    @contact = Contact.create(:name => params[:name],
+                              :email => params[:email],
+                              :phone => params[:phone])
+
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/index.html.erb')
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
   end
 
 end
